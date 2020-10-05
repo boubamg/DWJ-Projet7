f795031181cdb18e4af6517bf351ef124ecd0691
boubamg <bouba.maiga99@gmail.com>
2020-10-05 15:27:25 +0200

    deleting oneArticle component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,9 @@ import Signup from './components/User/Connexion/Signup';
 import Login from './components/User/Connexion/Login';
 import Profile from './components/User/Profile'
 import Articles from './components/Articles/Articles';
-import Article from './components/Articles/Article'; 
-import updateArticle from './components/Articles/updateArticle'; 
-import updateProfile from './components/User/updateProfile';
+import updateArticle from './components/Articles/updateArticle';
 
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
-import createComment from './components/Comments/createComment';
 
 
 class App extends Component {
@@ -34,7 +31,6 @@ class App extends Component {
             <Route exact path='/profile' component={Profile} />
 
             <Route exact path='/post' component={Articles} />
-            {/* <Route exact path='/post/:id' component={Article} /> */}
             <Route exact path='/post/update/:id' component={updateArticle} />
 
             
